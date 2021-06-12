1e19c576788cdb6bf3e2d2564100490f06200bd0
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-11 23:47:18 -0300

    Classes adicionadas

---
+++ OrderItemsEx/Entities/Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OrderItemsEx.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderItemsEx.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Orders { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem order)
+        {
+            Orders.Add(order);
+        }
+
+        public void RemoveItem(OrderItem order)
+        {
+            Orders.Remove(order);
+        }
+
+        public double Total()
+        {
+            var sum = 0.0;
+            
+            foreach(OrderItem order in Orders)
+            {
+                sum += order.SubTotal();
+            }
+
+            return sum;
+        }
+    }
+}
--- OrderItemsEx/Entities/Product.cs
+++ OrderItemsEx/Entities/Product.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OrderItemsEx.Entities
+﻿namespace OrderItemsEx.Entities
 {
     class Product
     {
