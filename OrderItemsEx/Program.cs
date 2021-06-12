1b4e9ff4bc2168e89d262b56983553b5ab9bdf34
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-11 23:18:14 -0300

    Projeto OrderItems iniciado

---
+++ OrderItemsEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderItemsEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
